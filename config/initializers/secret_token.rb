a273648a457cddd6a4d54113a6051ba4c08b842b
RL <rl@rl-VirtualBox.(none)>
2013-04-15 16:45:56 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TutApp::Application.config.secret_token = 'e770ce2e9380bd981f5b82e8e8c44f9ce716f2cdd47765bea1c8a507d9ea64ecd74f4507dee43397de0b1cb30b799e449f646a2db43bc10b73641ad5f7a8c0b5'
