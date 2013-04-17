f41843b0b4e6d10044b126e73527be4f48935631
RL <rl@rl-VirtualBox.(none)>
2013-04-17 13:08:42 +0200

    Web Page Layout added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+  	@title = "Home"
   end
 
   def help
+  	@title = "Help"
   end
 
   def about
+  	@title = "About Us"
   end
 
 end
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -3,25 +3,43 @@ require 'spec_helper'
 describe "Pages" do
  
  describe "Home page" do
-    it "should have the content 'home'" do
+    it "should have the content 'Home'" do
       visit '/pages/home'
-      page.should have_content('home')
+      page.should have_content('Home')
+    end
+
+    it "should have the right title" do
+      visit '/pages/home'
+      page.should have_selector('title', 
+                        :text => "Tutorial App | Home")
     end
  end
 
  describe "Help page" do
 
-    it "should have the content 'help'" do
+    it "should have the content 'Help'" do
       visit '/pages/help'
-      page.should have_content('help')
+      page.should have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/pages/help'
+      page.should have_selector('title',
+                        :text => "Tutorial App | Help")
     end
  end
 
  describe "About page" do
 
-    it "should have the content 'about'" do
+    it "should have the content 'About'" do
       visit '/pages/about'
-      page.should have_content('about')
+      page.should have_content('About')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/pages/about'
+      page.should have_selector('title',
+                        :text => "Tutorial App | About Us")
     end
  end
 
