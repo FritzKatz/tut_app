466f4d41e2f8bc0a2a8d9c0c2004b0751d966631
RL <rl@rl-VirtualBox.(none)>
2013-04-17 18:03:31 +0200

    Blueprint CSS Framework added

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,11 @@
 module PagesHelper
+	# Provide / Return a general title on a per page basis
+	def title
+		base_title = "Tutorial App"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
 end
