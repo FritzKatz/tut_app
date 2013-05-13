106ca5ff34ed627f557fe0b4111baa09b7cf13f0
RL <rl@rl-VirtualBox.(none)>
2013-05-13 13:42:34 +0200

    Implemented user deit, update, index and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,15 @@
 class SessionsController < ApplicationController
 
 	def new
-		@title = "Sign in"
-
+    @title = "Sign in"
 	end
 
 	def create
+    @title = "Sign in"
 	  user = User.find_by_email(params[:session][:email].downcase)
   	  if user && user.authenticate(params[:session][:password])
       	sign_in user
-      	redirect_to user
+      	redirect_back_or user
       else
       	flash.now[:error] = 'Invalid email/password combination'
       	render 'new'
